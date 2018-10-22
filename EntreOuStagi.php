6087529dd7fb0228488fface33a8c40c18cdd3ad
coda2018 <codaern@gmail.com>
2018-10-22 08:31:31 +0200

    form

---
+++ EntreOuStagi.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet" type="text/css">
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Fil_rouge</title>
+  </head>
+  <body>
+
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <center><img src="img/IMG_0759.jpg" class="img-responsive"></center>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div class="inscription"><h2><U style="color: #E80F00;">Inscription</U></h2></div>
+                </article>
+            </div>
+            <div class="form-check-label1">
+                <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked=""> Stagiaire</label>
+            </div>
+            <div class="form-check-label2">
+                <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2" checked=""> Entreprise</label>
+            </div>
+            <center><div class="valider01"><button class="btn btn-success"> Valider </button></div></center>
+        </div>
+
+    </div>
+    <script type="text/javascript" src="js/bootstrap.min.js">
+
+    </script>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ inscription_entreprise.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet" type="text/css">
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Fil_rouge</title>
+  </head>
+  <body>
+
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <center><img src="img/IMG_0759.jpg" class="img-responsive"></center>
+                    <div class="inscription" style="padding-bottom: 5%"><h2><U style="color: #E80F00;">Inscription</U></h2></div>
+
+                <form class="form01">
+                    <div class="form-group">
+                        <label for="nom_entreprise">Nom de l'Entreprise</label>
+                        <input type="text" id="nom_entreprise" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="siret">Siret de l'Entreprise</label>
+                        <input type="text" id="siret" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="mail">E-mail</label>
+                        <input type="text" id="mail" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="valider_mail">Confirmer E-mail</label>
+                        <input type="text" id="valider_mail" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="mot_de_passe">Mot de passe</label>
+                        <input type="text" id="mot_de_passe" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="confirmer_mdp">Confirmer le mot de passe</label>
+                        <input type="text" id="confirmer_mdp" class="form-control">
+                        </div>
+                        <label><input type="checkbox" value="">J'accepte les conditions général d'utilisation</label>
+                    </div>
+                </form>
+            </div>
+            <center><div class="valider"><button class="btn btn-success"> Valider </button></div></center>
+        </div>
+
+    </div>
+    <script type="text/javascript" src="js/bootstrap.min.js">
+
+    </script>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
