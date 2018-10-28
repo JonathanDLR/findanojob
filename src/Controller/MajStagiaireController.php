bf14455b180ee394058f5f9b077f962514a0e236
JonathanDLR <marie89>
2018-10-28 19:20:49 +0100

    build

---
+++ src/Controller/MajStagiaireController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\Type\StagiaireType;
+use App\Form\Handler\MajStagiaireHandler;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MajStagiaireController extends AbstractController
+{
+    /**
+     * @Route("/account/{id}", name="account")
+     */
+    public function majstagiaire($id, MajStagiaireHandler $formHandler, Request $request): Response
+    {
+        $formStagMaj = $this->createForm(StagiaireType::class);
+        $stagiaire = $this->getDoctrine()->getRepository('App:Stagiaire')->find($id);
+
+        if($formHandler->handle($formStagMaj, $request)) {
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('account.html.twig', [
+            'formStagMaj' => $formStagMaj->createView(),
+            'stagiaire' => $stagiaire
+        ]);
+    }
+}
---
+++ src/Form/Handler/MajStagiaireHandler.php
@@ -0,0 +1,70 @@
+<?php
+namespace App\Form\Handler;
+
+use App\Entity\Stagiaire;
+use App\Entity\User;
+use App\Model\Stagiaire as StagiaireModel;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+
+
+class MajStagiaireHandler
+{
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(ObjectManager $objectManager, EntityManagerInterface $em, Security $security)
+    {
+        $this->objectManager = $objectManager;
+        $this->em = $em;
+        $this->security = $security;
+    }
+
+    public function handle(FormInterface $form, Request $request)
+    {
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $stagiaireModel = $form->getData();
+            $user = $this->security->getUser();
+            $id = $user->getId();
+            $stagiaire = $this->em->getRepository('App:Stagiaire')->find($id);
+
+            $stagiaire->setNom($stagiaireModel->nom);
+            $stagiaire->setPrenom($stagiaireModel->prenom);
+            $stagiaire->setAdresse($stagiaireModel->adresse);
+            $stagiaire->setVille($stagiaireModel->ville);
+            $stagiaire->setDepartement($stagiaireModel->departement);
+            $stagiaire->setRegion($stagiaireModel->region);
+            $stagiaire->setDateDebut($stagiaireModel->date_debut);
+            $stagiaire->setDateFin($stagiaireModel->date_fin);
+            $stagiaire->setAge($stagiaireModel->age);
+            $stagiaire->setSexe($stagiaireModel->sexe);
+            $stagiaire->setPresentation($stagiaireModel->presentation);
+            $stagiaire->setUser($user);
+
+            $this->objectManager->flush();
+        }
+    }
+}
+
+?>
